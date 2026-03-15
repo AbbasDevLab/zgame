15d27c106c2844abed849758e6aa1084c1bd9554
AbbasDevLab <haider_abbas_official@yahoo.com>
2026-03-15 06:13:08 +0500

    Change export syntax in chat.js from ES6 to CommonJS module format

--- api/chat.js
+++ api/chat.js
@@ -46,7 +46,7 @@ function readBody(req) {
   });
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
